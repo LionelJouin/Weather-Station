9d0d3463533f95e69ca0e3a3d0828638101466b2
Lionel Jouin <lionel.jouin@outlook.com>
2018-03-07 19:46:35 +0100

    autoupdate

--- server/boot/automigrate.js
+++ server/boot/automigrate.js
@@ -5,9 +5,25 @@ module.exports = function (app) {
     var ds = app.dataSources.AzureDB;
     var lbTables = ["User", "AccessToken", "ACL", "RoleMapping", "Role", "Station", "Sensor", "StationSensor"];
 
-    ds.automigrate(lbTables, function (err) {
-        if (err) throw err;
-        console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
-    });
+    // migrate();
+    update();
+
+    function migrate() {
+        ds.automigrate(lbTables, function (err) {
+            if (err) throw err;
+            console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+        });
+    }
+
+    function update() {
+        ds.isActual(lbTables, function (err, actual) {
+            if (!actual) {
+                ds.autoupdate(lbTables, function (err, result) {
+                    if (err) throw err;
+                    console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+                });
+            }
+        });
+    }
 
 }
